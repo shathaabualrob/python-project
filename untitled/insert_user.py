b52c31c5db6baf77ca97f992296c1d18fce29a61
Amal Motaz Irshiad <49159257+AmalIrshiad@users.noreply.github.com>
2020-01-02 21:10:47 +0200

    9:10

--- untitled/insert_user.py
+++ untitled/insert_user.py
@@ -6,9 +6,17 @@ import os
 
 
 class insert_user(Resource):
-    def insert_user(id,name,country,age,email,password):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("Id", required=True, type=int)
+        parser.add_argument("Name", required=True)
+        parser.add_argument("Age",required=True)
+        parser.add_argument("Country",required=True)
+        parser.add_argument("Email",required=True)
+        parser.add_argument("Password",required=True)
+        content = parser.parse_args()
         connection, cursor = ConnectionHelper().get_connection()
-        query = "insert into employee(id,name,country,age,email,password) values(" + str(id) + ",'" + name + "')"
+        query = "insert into User(Id,name,Country,Age,Email,Password) values(" + str(content["Id"]) + ",'" + str(content["Name"]) + ",'" + str(content["Age"]) +",'" + str(content["Country"]) + ",'" + str(content["Email"]) +",'" + str(content["Password"]) +"')"
         cursor.execute(query)
         connection.commit()
 
